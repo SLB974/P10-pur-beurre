fd992b8c89859ea3f5640d048144edb44267549c
SLB974 <slb974run@gmail.com>
2022-06-06 11:37:26 +0400

    adding raven to requirements.txt

--- app/django_p8/settings.py
+++ app/django_p8/settings.py
@@ -14,8 +14,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
+# import sentry_sdk
+# from sentry_sdk.integrations.django import DjangoIntegration
 
 import raven
 
